6c487e964894b92ee0374a9a33f5e2bd303dc99f
Alif Aditya <mochammadalifa7@gmail.com>
2024-01-24 01:29:16 +0700

    tambah page koleksi

---
+++ view/koleksi.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+
+    <title>Perpuzz</title>
+    <meta content="" name="description" />
+    <meta content="" name="keywords" />
+
+    <!-- Favicons -->
+    <link href="../assets/img/favicon.png" rel="icon" />
+    <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
+
+    <!-- Google Fonts -->
+    <link
+      href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700"
+      rel="stylesheet"
+    />
+
+    <!-- Vendor CSS Files -->
+    <link href="../assets/vendor/aos/aos.css" rel="stylesheet" />
+    <link
+      href="../assets/vendor/bootstrap/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <link
+      href="../assets/vendor/bootstrap-icons/bootstrap-icons.css"
+      rel="stylesheet"
+    />
+    <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet" />
+    <link
+      href="../assets/vendor/glightbox/css/glightbox.min.css"
+      rel="stylesheet"
+    />
+    <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
+
+    <!-- Template Main CSS File -->
+    <link href="../assets/css/style.css" rel="stylesheet" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" />
+
+  </head>
+
+  <body>
+    <!-- ======= Header ======= -->
+    <header
+      id="header"
+      class="fixed-top d-flex align-items-center header-koleksi"
+    >
+      <div class="container d-flex justify-content-between align-items-center">
+        <div id="logo">
+          <a href="index.html"><img src="../assets/img/logo.png" alt="" /></a>
+          <!-- Uncomment below if you prefer to use a text logo -->
+          <!--<h1><a href="index.html">Regna</a></h1>-->
+        </div>
+      
+        <nav id="navbar" class="navbar">
+          <ul>
+            <li><a class="nav-link" href="#hero">Home</a></li>
+            <li><a class="nav-link active" href="koleksi.php">Collection</a></li>
+            <li><a class="nav-link" href="#about">Catalog</a></li>
+            <li><a class="nav-link" href="#services">Favorite</a></li>
+            </ul>
+          <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav>
+        <!-- .navbar -->
+      </div>
+    </header>
+    <!-- End Header -->
+
+    <!-- ======= Section ======= -->
+    <section id="koleksi">
+      <div class="koleksi-container pt-5" data-aos="zoom-in" data-aos-delay="100">
+        <center><h1>Koleksi Buku</h1></center>
+      </div>
+    </section>
+    <!-- End Section -->
+
+    <!-- content -->
+    <div class="container px-3">
+      <div class="row justify-content-md-left justify-content-sm-left">
+        <div class="col-md-3 col-sm-6">
+          <div class="product-grid">
+              <div class="product-image">
+                  <a href="#" class="image">
+                      <img class="pic-1" src="../assets/img/gmbr (1).jpeg">
+                      <img class="pic-2" src="../assets/img/gmbr (2).jpg">
+                  </a>
+                  <a href="#" class="product-like-icon" data-tip="Add to Wishlist">
+                      <i class="far fa-heart"></i>
+                  </a>
+                  <ul class="product-links">
+                      <li><a href="#"><i class="fa fa-search"></i></a></li>
+                      <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                      
+                  </ul>
+              </div>
+              <div class="product-content">
+                  <h3 class="title"><a href="#">strategi visual</a></h3>
+                  
+              </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid">
+                <div class="product-image">
+                    <a href="#" class="image">
+                        <img class="pic-1" src="../assets/img/gmbr (3).jpg">
+                        <img class="pic-2" src="../assets/img/gmbr (4).jpg">
+                    </a>
+                    <span class="product-sale-label">Sale</span>
+                    <a href="#" class="product-like-icon" data-tip="Add to Wishlist">
+                        <i class="far fa-heart"></i>
+                    </a>
+                    <ul class="product-links">
+                        <li><a href="#"><i class="fa fa-search"></i></a></li>
+                        <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                        
+                    </ul>
+                </div>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">aplikasi internet</a></h3>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid">
+                <div class="product-image">
+                    <a href="#" class="image">
+                        <img class="pic-1" src="../assets/img/gmbr (5).jpeg">
+                        <img class="pic-2" src="../assets/img/gmbr (6).jpg">
+                    </a>
+                    <a href="#" class="product-like-icon" data-tip="Add to Wishlist">
+                        <i class="far fa-heart"></i>
+                    </a>
+                    <ul class="product-links">
+                        <li><a href="#"><i class="fa fa-search"></i></a></li>
+                        <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                        
+                    </ul>
+                </div>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">filsafat ilmu</a></h3>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+          <div class="product-grid">
+              <div class="product-image">
+                  <a href="#" class="image">
+                      <img class="pic-1" src="../assets/img/gmbr (2).jpg">
+                      <img class="pic-2" src="../assets/img/gmbr (1).jpeg">
+                  </a>
+                  <a href="#" class="product-like-icon" data-tip="Add to Wishlist">
+                      <i class="far fa-heart"></i>
+                  </a>
+                  <ul class="product-links">
+                      <li><a href="#"><i class="fa fa-search"></i></a></li>
+                      <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                      
+                  </ul>
+              </div>
+              <div class="product-content">
+                  <h3 class="title"><a href="#">rasa</a></h3>
+                  
+              </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid">
+                <div class="product-image">
+                    <a href="#" class="image">
+                        <img class="pic-1" src="../assets/img/gmbr (4).jpg">
+                        <img class="pic-2" src="../assets/img/gmbr (3).jpg">
+                    </a>
+                    <span class="product-sale-label">Sale</span>
+                    <a href="#" class="product-like-icon" data-tip="Add to Wishlist">
+                        <i class="far fa-heart"></i>
+                    </a>
+                    <ul class="product-links">
+                        <li><a href="#"><i class="fa fa-search"></i></a></li>
+                        <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                        
+                    </ul>
+                </div>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">geografi kota dan desa</a></h3>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid">
+                <div class="product-image">
+                    <a href="#" class="image">
+                        <img class="pic-1" src="../assets/img/gmbr (6).jpg">
+                        <img class="pic-2" src="../assets/img/gmbr (5).jpeg">
+                    </a>
+                    <a href="#" class="product-like-icon" data-tip="Add to Wishlist">
+                        <i class="far fa-heart"></i>
+                    </a>
+                    <ul class="product-links">
+                        <li><a href="#"><i class="fa fa-search"></i></a></li>
+                        <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                        
+                    </ul>
+                </div>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">penelitian hukum</a></h3>
+                    
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+    <br>
+    <!-- end content -->
+    <!-- panel navigation page -->
+    <div class="container">
+      <nav aria-label="..." >
+        <ul class="pagination justify-content-center">
+          <li class="page-item disabled">
+            <a class="page-link">Previous</a>
+          </li>
+          <li class="page-item active" aria-current="page"><a class="page-link" href="#">1</a></li>
+          <li class="page-item"><a class="page-link" href="#">2</a></li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item"><a class="page-link" href="#">Next</a></li>
+        </ul>
+      </nav>
+    </div>
+    <!-- end panel -->
+    <!-- Vendor JS Files -->
+    <script src="../assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="../assets/vendor/aos/aos.js"></script>
+    <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="../assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="../assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="../assets/js/main.js"></script>
+  </body>
+</html>
