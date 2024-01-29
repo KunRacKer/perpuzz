69adb5dea65f451b990094a46cb9c035f021d58e
Alif Aditya <mochammadalifa7@gmail.com>
2024-01-30 01:51:26 +0700

    benerin koleksi

--- view/koleksi.php
+++ view/koleksi.php
@@ -83,162 +83,31 @@
     </section>
     <!-- End Section -->
 
-    <!-- content -->
-    <div class="container px-3" data-aos="zoom-in" data-aos-delay="100">
-      <div class="row justify-content-md-left justify-content-sm-left">
-        <div class="col-md-3 col-sm-6">
-          <div class="product-grid">
-              <div class="product-image">
-                  <a href="#" class="image">
-                      <img class="pic-1" src="../assets/img/gmbr (1).jpeg">
-                      <img class="pic-2" src="../assets/img/black.jpg">
-                  </a>
-                  <a href="#" class="product-like-icon" data-tip="Add to Wishlist">
-                      <i class="fa fa-heart"></i>
-                  </a>
-                  <ul class="product-links">
-                      <li><a href="#"><i class="fa fa-search"></i></a></li>
-                      <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                      
-                  </ul>
-              </div>
-              <div class="product-content">
-                  <h3 class="title"><a href="#">strategi visual</a></h3>
-                  
-              </div>
-          </div>
-        </div>
-        <div class="col-md-3 col-sm-6">
-            <div class="product-grid">
-                <div class="product-image">
-                    <a href="#" class="image">
-                        <img class="pic-1" src="../assets/img/gmbr (3).jpg">
-                        <img class="pic-2" src="../assets/img/black.jpg">
-                    </a>
-                    <span class="product-sale-label">Sale</span>
-                    <a href="#" class="product-like-icon" data-tip="Add to Wishlist">
-                        <i class="fa fa-heart"></i>
-                    </a>
-                    <ul class="product-links">
-                        <li><a href="#"><i class="fa fa-search"></i></a></li>
-                        <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                        
-                    </ul>
+<?php
+// fetching data
+include '../forms/koneksi.php';
+$query = "SELECT Judul, Gambar FROM buku ORDER BY Judul ASC";
+$result = mysqli_query($koneksi, $query);
+?>
+        <!-- content -->
+        <div class="container px-3" data-aos="zoom-in" data-aos-delay="100">
+            <div class="row">
+                <?php while ($d = mysqli_fetch_object($result)) { ?>
+                <div class="col-md-3 col-sm-6">
+                    <div class="card my-4"><a href="#">
+                        <img src="../assets/img/sampul/<?= $d->Gambar; ?>" alt="<?= $d->Judul; ?>" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $d->Judul; ?></h5>
+                        </div>
+                    </a></div>
                 </div>
-                <div class="product-content">
-                    <h3 class="title"><a href="#">aplikasi internet</a></h3>
-                    
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3 col-sm-6">
-            <div class="product-grid">
-                <div class="product-image">
-                    <a href="#" class="image">
-                        <img class="pic-1" src="../assets/img/gmbr (5).jpeg">
-                        <img class="pic-2" src="../assets/img/black.jpg">
-                    </a>
-                    <a href="#" class="product-like-icon" data-tip="Add to Wishlist">
-                        <i class="fa fa-heart"></i>
-                    </a>
-                    <ul class="product-links">
-                        <li><a href="#"><i class="fa fa-search"></i></a></li>
-                        <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                        
-                    </ul>
-                </div>
-                <div class="product-content">
-                    <h3 class="title"><a href="#">filsafat ilmu</a></h3>
-                    
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3 col-sm-6">
-          <div class="product-grid">
-              <div class="product-image">
-                  <a href="#" class="image">
-                      <img class="pic-1" src="../assets/img/gmbr (2).jpg">
-                      <img class="pic-2" src="../assets/img/black.jpg">
-                  </a>
-                  <a href="#" class="product-like-icon" data-tip="Add to Wishlist">
-                      <i class="fa fa-heart"></i>
-                  </a>
-                  <ul class="product-links">
-                      <li><a href="#"><i class="fa fa-search"></i></a></li>
-                      <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                      
-                  </ul>
-              </div>
-              <div class="product-content">
-                  <h3 class="title"><a href="#">rasa</a></h3>
-                  
-              </div>
-          </div>
-        </div>
-        <div class="col-md-3 col-sm-6">
-            <div class="product-grid">
-                <div class="product-image">
-                    <a href="#" class="image">
-                        <img class="pic-1" src="../assets/img/gmbr (4).jpg">
-                        <img class="pic-2" src="../assets/img/black.jpg">
-                    </a>
-                    <span class="product-sale-label">Sale</span>
-                    <a href="#" class="product-like-icon" data-tip="Add to Wishlist">
-                        <i class="fa fa-heart"></i>
-                    </a>
-                    <ul class="product-links">
-                        <li><a href="#"><i class="fa fa-search"></i></a></li>
-                        <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                        
-                    </ul>
-                </div>
-                <div class="product-content">
-                    <h3 class="title"><a href="#">geografi kota dan desa</a></h3>
-                    
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3 col-sm-6">
-            <div class="product-grid">
-                <div class="product-image">
-                    <a href="#" class="image">
-                        <img class="pic-1" src="../assets/img/gmbr (6).jpg">
-                        <img class="pic-2" src="../assets/img/black.jpg">
-                    </a>
-                    <a href="#" class="product-like-icon" data-tip="Add to Wishlist">
-                        <i class="fa fa-heart"></i>
-                    </a>
-                    <ul class="product-links">
-                        <li><a href="#"><i class="fa fa-search"></i></a></li>
-                        <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                        
-                    </ul>
-                </div>
-                <div class="product-content">
-                    <h3 class="title"><a href="#">penelitian hukum</a></h3>
-                    
-                </div>
+                <?php } ?>
             </div>
         </div>
       </div>
     </div>
     <br>
     <!-- end content -->
-    <!-- panel navigation page -->
-    <div class="container">
-      <nav aria-label="..." >
-        <ul class="pagination justify-content-center">
-          <li class="page-item disabled">
-            <a class="page-link">Previous</a>
-          </li>
-          <li class="page-item active" aria-current="page"><a class="page-link" href="#">1</a></li>
-          <li class="page-item"><a class="page-link" href="#">2</a></li>
-          <li class="page-item"><a class="page-link" href="#">3</a></li>
-          <li class="page-item"><a class="page-link" href="#">Next</a></li>
-        </ul>
-      </nav>
-    </div>
-    <!-- end panel -->
     <!-- Vendor JS Files -->
     <script src="../assets/vendor/purecounter/purecounter_vanilla.js"></script>
     <script src="../assets/vendor/aos/aos.js"></script>
