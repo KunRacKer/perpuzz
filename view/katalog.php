69301e17f4638b15e9f8183fdf920e66d9df4e70
kirinosuki <Septian070903@gmail.com>
2024-01-31 16:25:50 +0700

    edit catalog

---
+++ view/katalog.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+
+    <title>Perpuzz</title>
+    <meta content="" name="description" />
+    <meta content="" name="keywords" />
+
+    <!-- Favicons -->
+    <link href="../assets/img/favicon.png" rel="icon" />
+    <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
+
+    <!-- Google Fonts -->
+    <link
+      href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700"
+      rel="stylesheet"
+    />
+
+    <!-- Vendor CSS Files -->
+    <link href="../assets/vendor/aos/aos.css" rel="stylesheet" />
+    <link
+      href="../assets/vendor/bootstrap/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <link
+      href="../assets/vendor/bootstrap-icons/bootstrap-icons.css"
+      rel="stylesheet"
+    />
+    <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet" />
+    <link
+      href="../assets/vendor/glightbox/css/glightbox.min.css"
+      rel="stylesheet"
+    />
+    <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
+
+    <!-- Template Main CSS File -->
+    <link href="../assets/css/style.css" rel="stylesheet" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" />
+
+  </head>
+
+    <!-- ======= Header ======= -->
+    <header
+      id="header"
+      class="fixed-top d-flex align-items-center header-koleksi"
+    >
+      <div class="container d-flex justify-content-between align-items-center">
+        <div id="logo">
+          <a href="index.html"><img src="../assets/img/logo.png" alt="" /></a>
+          <!-- Uncomment below if you prefer to use a text logo -->
+          <!--<h1><a href="index.html">Regna</a></h1>-->
+        </div>
+      
+        <nav id="navbar" class="navbar">
+          <ul>
+            <li><a class="nav-link" href="../index.php">Home</a></li>
+            <li><a class="nav-link" href="koleksi.php">Collection</a></li>
+            <li><a class="nav-link active" href="#about">Catalog</a></li>
+            <li><a class="nav-link" href="#services">Favorite</a></li>
+            </ul>
+          <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav>
+        <!-- .navbar -->
+      </div>
+    </header>
+    <!-- end header -->
+
+    <style>
+    .scrollable-div {
+    height: 600px;
+    overflow-y: scroll;
+
+    }
+    .tg-widgetcontent ul li:before {
+			
+            background: #fff; 
+           
+       }
+    
+    .tg-widgetcontent ul li a {
+		color: #085ca3;
+
+	}
+
+    .tg-widgetcontent ul li {
+		padding: 10px 10px 10px 9px;
+		list-style:outside none none;
+	}
+</style>
+
+<!-- Koleksi+widget -->
+<div class="container">
+<div class="row">
+    
+<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6" >
+ <h1> Koleksi Terbaru</h1>
+ <hr>
+ 
+  <div class="scrollable-div" style="max-height:600px;overflow-y:scroll;">
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto" style="font-size: 12px;">
+    <a href="#" class="list-group-item list-group-item-action" style="color: blue;">A second link item</a>
+      Content for list item
+    </div>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto" style="font-size: 12px;">
+    <a href="#" class="list-group-item list-group-item-action" style="color: blue;">A second link item</a>
+      Content for list item
+    </div> 
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto" style="font-size: 12px;">
+    <a href="#" class="list-group-item list-group-item-action" style="color: blue;">A second link item</a>
+      Content for list item
+    </div>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto" style="font-size: 12px;">
+    <a href="#" class="list-group-item list-group-item-action" style="color: blue;">A second link item</a>
+      Content for list item
+    </div>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto" style="font-size: 12px;">
+    <a href="#" class="list-group-item list-group-item-action" style="color: blue;">A second link item</a>
+      Content for list item
+    </div>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto" style="font-size: 12px;">
+    <a href="#" class="list-group-item list-group-item-action" style="color: blue;">A second link item</a>
+      Content for list item
+    </div>
+  </li>
+</div>
+    </div>
+ </div>
+</div>
+<!-- end  -->
+
+<!-- koleksi 2 -->
+<div class="container">
+ <div class="row"> 
+<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6" >
+	<div class="tg-widget tg-widgetdownload " style="padding:20px;background-color:#fff;">
+									
+		<div class="tg-widgettitle">
+			<h3>Koleksi</h3>
+		</div>
+	    <div class="tg-widgetcontent" style="padding:0px 10px;">
+	<ul>
+	<li><a href="#">Buku</a><span class="pull-right"> # judul, # eksemplar.</span></li><li><a href="#">Audio Visual</a><span class="pull-right"># judul, # eksemplar.</span></li><li><a href="#">Berkala / Serial</a><span class="pull-right"># judul, # eksemplar.</span></li></ul>
+				</div>
+			    </div>
+										
+			<div class="tg-widget tg-widgetdownload" style="padding:20px;background-color:#fff;">
+										
+				<div class="tg-widgettitle">
+					<h3>Grup Jenis Media</h3>
+				</div>
+				<div class="tg-widgetcontent" id="grup_jenis_media" style="padding:0px 10px;">
+	<ul>
+	<li><a href="#">Art Original</a> <span class="pull-right"># judul , # eksemplar.</span></li><li><a href="#">CD-ROM</a> <span class="pull-right"># judul , # eksemplar.</span></li><li><a href="#">Diskete</a> <span class="pull-right"># judul , # eksemplar.</span></li><li><a href="#">Electronic Resource</a> <span class="pull-right"># judul , # eksemplar.</span></li><li><a href="#">KASSETE</a> <span class="pull-right"># judul , # eksemplar.</span></li><li><a href="#">Text book</a> <span class="pull-right"># judul , # eksemplar.</span></li></ul>
+				</div>							
+				</div>
+										
+			<div class="tg-widget tg-widgetdownload" style="padding:20px;background-color:#fff;">
+										
+				<div class="tg-widgettitle">
+					<h3>Grup Jenis Serial</h3>
+				</div>
+				<div class="tg-widgetcontent" id="grup_jenis_serial" style="padding:0px 10px;">
+	<ul>
+	<li><a href="#">Buletin</a> <span class="pull-right"># judul , # eksemplar.</span></li><li><a href="#">Jurnal</a> <span class="pull-right"># judul , # eksemplar.</span></li><li><a href="#">Kliping</a> <span class="pull-right"># judul , # eksemplar.</span></li><li><a href="#">Koran</a> <span class="pull-right"># judul , # eksemplar.</span></li><li><a href="#">Laporan Kegiatan</a> <span class="pull-right"># judul , # eksemplar.</span></li><li><a href="#">Majalah</a> <span class="pull-right"># judul , # eksemplar.</span></li><li><a href="#">Prosiding</a> <span class="pull-right"># judul , # eksemplar.</span></li><li><a href="#">Tabloid</a> <span class="pull-right"># judul , # eksemplar.</span></li></ul>
+				</div>
+				</div>
+</div>
+ </div>
+  </body>
+</html>
