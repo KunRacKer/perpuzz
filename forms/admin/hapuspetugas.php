d8b7af8a6a11f91cce3a267aa43b166e6d14387f
Alif Aditya <mochammadalifa7@gmail.com>
2024-02-05 19:59:27 +0700

    update terakhir

--- forms/admin/hapuspetugas.php
+++ forms/admin/hapuspetugas.php
@@ -5,11 +5,17 @@ include '../koneksi.php';
 if (isset($_GET['ID_petugas'])) {
     $id = $_GET['ID_petugas'];
     
-    // Perform the delete operation
-    $delete_query = "DELETE FROM petugas WHERE ID_petugas = '$id' ";
-    if (mysqli_query($koneksi, $delete_query)) {
+    // Membuat prepared statement
+    $stmt1 = $koneksi->prepare("DELETE FROM petugas WHERE ID_petugas = ?");
+    $stmt1->bind_param("s", $id); // s = string, i = integer, d = digit/real
+
+    $stmt2 = $koneksi->prepare("DELETE FROM user WHERE ID_petugas = ?");
+    $stmt2->bind_param("s", $id);
+
+    // Eksekusi prepared statement dan cek hasil
+    if ($stmt1->execute() && $stmt2->execute()) {
         // Redirect back to the Meja.php page after successful deletion
-        header('Location: ../../view/admin/petugas.php');
+        header("Location: ../../view/admin/petugas.php");
         exit;
     } else {
         echo "Error deleting record: " . mysqli_error($koneksi);
@@ -17,4 +23,6 @@ if (isset($_GET['ID_petugas'])) {
 } else {
     echo "No record to delete.";
 }
+
+mysqli_close($koneksi);
 ?>
--- forms/admin/hapususer.php
+++ forms/admin/hapususer.php
@@ -5,11 +5,17 @@ include '../koneksi.php';
 if (isset($_GET['ID_anggota'])) {
     $id = $_GET['ID_anggota'];
     
-    // Perform the delete operation
-    $delete_query = "DELETE FROM anggota WHERE ID_anggota = '$id' ";
-    if (mysqli_query($koneksi, $delete_query)) {
+    // Membuat prepared statement
+    $stmt1 = $koneksi->prepare("DELETE FROM anggota WHERE ID_anggota = ?");
+    $stmt1->bind_param("s", $id); // s = string, i = integer, d = digit/real
+
+    $stmt2 = $koneksi->prepare("DELETE FROM user WHERE ID_anggota = ?");
+    $stmt2->bind_param("s", $id);
+
+    // Eksekusi prepared statement dan cek hasil
+    if ($stmt1->execute() && $stmt2->execute()) {
         // Redirect back to the Meja.php page after successful deletion
-        header('Location: ../../view/admin/user.php');
+        header("Location: ../../view/admin/user.php");
         exit;
     } else {
         echo "Error deleting record: " . mysqli_error($koneksi);
@@ -17,4 +23,6 @@ if (isset($_GET['ID_anggota'])) {
 } else {
     echo "No record to delete.";
 }
+
+mysqli_close($koneksi);
 ?>
