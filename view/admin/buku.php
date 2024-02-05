add85d44f36b32ea9766ddc16933422e618fc0fe
KunRacKer <razero246@gmail.com>
2024-02-05 12:16:37 +0700

    ubah link admin php

--- view/admin/buku.php
+++ view/admin/buku.php
@@ -39,7 +39,7 @@
         <ul class="navbar-na bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="admin.php">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
